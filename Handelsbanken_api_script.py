1e3be6dea8fc83faf393d0e781da9f29425ad413
Andrejs <andrejs.boja@gmail.com>
2021-07-09 16:23:11 +0300

    script_fix

--- Handelsbanken_api_script.py
+++ Handelsbanken_api_script.py
@@ -2,10 +2,10 @@ import requests
 import json
 
 getAccounts = "https://sandbox.handelsbanken.com/openbanking/psd2/v2/accounts"
-getTransactions = "https://sandbox.handelsbanken.com/openbanking/psd2/v2/accounts/ae5883c0-6cf3-11e9-9c41-e957ce7d7d69/transactions"
+getTransactions = "https://sandbox.handelsbanken.com/openbanking/psd2/v2/accounts/ae58d1e0-6cf3-11e9-9c41-e957ce7d7d69/transactions"
 
 headers = {
-    'Authorization': 'Bearer REPLACE_TOKEN',
+    'Authorization': 'Bearer MTQ0NjJkZmQ5OTM2NDE1ZTZjNGZmZjI2',
     'PSU-IP-Address': '192.102.28.2',
     'TPP-Request-ID': 'c8271b81-4229-5a1f-bf9c-758f11c1f5b1',
     'TPP-Transaction-ID': '6b24ce42-237f-4303-a917-cf778e5013d6',
--- SEB_api_script.py
+++ SEB_api_script.py
@@ -5,10 +5,10 @@ transactions = "https://api-sandbox.sebgroup.com/ais/v7/identified2/accounts/6e3
 
 headers = {
   'Accept': 'application/json',
-  'X-Request-ID': '35fb5b7a-a183-454f-9cc5-f6bdb7d68e78',
-  'PSU-IP-Address': '130.55.114.104',
-  'Authorization': 'Bearer REPLACE_TOKEN',
-  'Cookie': 'C0WNET=17c40a6c-60de-444a-a478-196e74575c08'
+  'X-Request-ID': '9c61f71e-eb40-4f0a-aeba-a71c7e6a6bba',
+  'PSU-IP-Address': '14.212.5.47',
+  'Authorization': 'Bearer zhj25iD1rASdXzvvNadY',
+  'Cookie': 'C0WNET=17c40a6c-60de-444a-a478-196e74575c08; AMWEBJCT!%2Fmga!JSESSIONID=0000QXS2kmsndv8nRqf-cT8HsGi:938ed943-3c20-4801-ace0-52dd1dd31f86; PD-SESSION-ID=1_4_1_74dAcAo1Ei7BEeg9Nm+V5sAwRGsCjeiFjclFCf1Uu7rSpUPp; PGNET=77ab66f5-60e8-49c3-a15f-4ad27ccf2204; TS01136fc4=0107224bedcd55b1550d27fb97ae585dc525bb2d9e2b8621abd96bcccf6812f5668a3588076cc3154c81feb2399d6efc241b9017bc; TS018787d1=0107224bed09a67cc3d374acabcad9aec8d2d28ac43728a2e55aad0ce8d060a23b94ce46b28145b0e89a820f7a2e0a5124e9f1997a'
 }
 
 response = requests.request("GET", transactions, headers=headers)
